75391c24da79e32c8d7dd2c1645f261987204a06
Jerry Sievert <code@legitimatesounding.com>
2013-04-02 14:50:12 -0700

    add authentication

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var geocode = require('./lib/geocode');
+var geocode        = require('./lib/geocode'),
+    authentication = require('./lib/authentication');
 
 function ArcGIS (options) {
   this.options = options;
 
   this.geocode = geocode.geocode;
+  this.authenticate = authentication.authenticate;
 }
 
 exports.ArcGIS = ArcGIS;
