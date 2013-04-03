0670cb4d1bdef1cb4f43bed68a0cbdefaec75a22
chelm <christopher_helm@esri.com>
2013-04-03 15:11:52 -0600

    adding very very basic featureservice request support

---
+++ test/featureservice-test.js
@@ -0,0 +1,17 @@
+var vows   = require('vows');
+var assert = require('assert');
+
+var featureservice = require('../src/featureservice');
+
+vows.describe('FeatureService').addBatch({
+  'When requesting a featureservice': {
+    topic: function () {
+      featureservice.FeatureService( 'http://servicesdev.arcgis.com/f126c8da131543019b05e4bfab6fc6ac/arcgis/rest/services', 'hospitals', { format: 'json' }, this.callback);
+    },
+    'It should return the correct service metadata': function (err, data) {
+      assert.equal(err, null);
+      assert.notEqual(data, null);
+      assert.equal(data.name, 'hospitals');
+    }
+  }
+}).export(module);
